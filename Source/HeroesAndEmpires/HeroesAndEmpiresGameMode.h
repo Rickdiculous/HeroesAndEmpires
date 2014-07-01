1ab9d04af3b577870bc624613d641c72074aeb3e
Rickdiculous <rwwwachow@ncsu.edu>
2014-07-01 04:21:30 -0400

    1.0
    First Set

---
+++ Source/HeroesAndEmpires/HeroesAndEmpiresGameMode.h
@@ -0,0 +1,14 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "HeroesAndEmpiresGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHeroesAndEmpiresGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+};
+
+
+
